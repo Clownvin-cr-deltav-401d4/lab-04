f471f87de7703307b83bce0b00ad822a14a88aa1
Calvin G Hall <123chall@gmail.com>
2019-09-19 16:46:47 -0500

    finished tests

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -11,6 +11,13 @@ describe('Categories Model', () => {
   // How might we repeat this to check on types?
   it('sanitize() returns undefined with missing requirements', () => {
     const schema = categories.schema;
+    expect(schema.fields).toBeTruthy();
+    expect(schema.fields.id).toBeTruthy();
+    expect(schema.fields.id.type).toBe('string');
+    expect(schema.fields.id.required).toBeTruthy();
+    expect(schema.fields.name).toBeTruthy();
+    expect(schema.fields.name.type).toBe('string');
+    expect(schema.fields.name.required).toBeTruthy();
     var testRecord = {};
     for (var field in schema) {
       if (schema[field].required) {
@@ -48,18 +55,33 @@ describe('Categories Model', () => {
   it('can delete(id) a category', () => {
     let obj = { name: 'Test Category' };
     return categories.create(obj)
-      .then(record => {
-        console.log(record);
-        expect(categories.get(record.id)).toBeTruthy();
-        expect(categories.get(record.id).id).toBeTruthy();
+      .then(async(record) => {
+        let fromDB = await categories.get(record.id);
+        expect(fromDB.length).toEqual(1);
+        fromDB = fromDB[0];
+        expect(fromDB).toBeTruthy();
+        expect(fromDB.id).toBeTruthy();
         return categories.delete(record.id)
-          .then(() => {
-            console.log(categories.schema);
-            expect(categories.get(record.id)).toBeFalsy();
+          .then(async() => {
+            let fromDB = await categories.get(record.id);
+            expect(fromDB.length).toEqual(0);
           });
-      })
+      });
   });
 
   //update()
+  it('can update(id, entry) a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then((record) => {
+        return categories.update(record.id, { name: 'Blah Blah Blah' })
+          .then(async(record) => {
+            let fromDB = await categories.get(record.id);
+            expect(fromDB.length).toEqual(1);
+            fromDB = fromDB[0];
+            expect(fromDB.name).toEqual('Blah Blah Blah');
+          });
+      });
+  });
 
 });
